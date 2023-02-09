88bbb0c9cbec2ef791a7a7e6f3289394f7262b39
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-09 12:47:48 +0200

    first test fix

--- src/Board.mjs
+++ src/Board.mjs
@@ -6,7 +6,19 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.board = []
+    this.board = this.init_board(width,height)
+  }
+
+  init_board(width,height) {
+    let board = [[]]
+    let array = []
+    for(let i = 0; i < width; i++) {
+      array[i] = '.'
+    }
+    for(let i = 0; i < height; i++) {
+      board.push(array)
+    }
+    return board
   }
 
 
@@ -14,14 +26,12 @@ export class Board {
     
     let board = ""
 
-    for (let i = 0; i < this.height; i++) {
+    for (let i = 1; i <= this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        board += "."
+        board += this.board[i][j]
       }
       board += "\n"
     }
-
-    console.log(board)
 
     return board;
   }
