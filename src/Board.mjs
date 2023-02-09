d40d32f03d13815f0c029529b6c4eae5785bae39
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-09 13:28:13 +0200

    it starts from the top middle

--- src/Board.mjs
+++ src/Board.mjs
@@ -13,14 +13,23 @@ export class Board {
     this.falling = true
   }
 
+  tick() {
+
+  }
+
 
   toString() {
     
     let board = ''
+    let middle = this.width % 2
 
-    for (let i = 1; i <= this.height; i++) {
+    for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        board += '.'
+        if (this.falling && i == 0 && j == middle) {
+          board += 'X'
+        } else {
+          board += '.'
+        }
       }
       board += "\n"
     }
