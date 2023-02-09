0fb2f3f31c9857552cb0bdb44f68f3ed55816330
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-09 12:11:34 +0200

    first test

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,28 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = []
   }
 
+
   toString() {
-    return "TODO";
+    
+    let board = ""
+
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        board += "."
+      }
+      board += "\n"
+    }
+
+    console.log(board)
+
+    return board;
   }
 }
