f9e90f8e11e49b9127b19793bc6b009dfe45fa91
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-14 18:06:03 +0200

    initial orientation

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,3 +1,22 @@
 export class RotatingShape {
+    shape
+
+    constructor(shape) {
+        this.shape = shape
+    }
+
+    toString() {
+
+        let modified = ''
+
+        for (let i = 0; i < this.shape.length; i++) {
+            console.log(this.shape[i])
+            if (this.shape[i] !== ' ') {
+                modified += this.shape[i]
+            }
+        }
+        
+        return modified+'\n'
+    }
     
 }
